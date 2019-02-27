42bba950c06056cf86e63389475a3e142fa785a4
andrefmrocha <andrefmrocha@live.com.pt>
2019-02-27 13:17:14 +0000

    Finished partitioning exercise

--- cal_fp01/src/Partitioning.cpp
+++ cal_fp01/src/Partitioning.cpp
@@ -7,23 +7,39 @@
 
 int s_recursive(int n,int k)
 {
-   return 1;
+   if(k == 1 || n == k)
+	   return 1;
+	return s_recursive(n - 1, k - 1) + k * s_recursive(n - 1, k);
 }
 
 int s_dynamic(int n,int k)
 {
-	return 1;
+	vector<int> vec(k, 1);
+	for(int i = 0; i < (n - k); i++){
+		for(int j = 0; j < k; j++){
+			vec[j] = (j + 1) * vec[j] + vec[j - 1];
+		}
+	}
+	return vec[k - 1];
 }
 
 
 int b_recursive(int n)
 {
-	return 0;
+	int b = 0;
+	for(int i = 1; i <= n; i++){
+		b+=s_recursive(n, i);
+	}
+	return b;
 }
 
 int b_dynamic(int n)
 {
-	return 0;
+	int b = 0;
+	for(int i = 1; i <= n; i++){
+		b+=s_dynamic(n, i);
+	}
+	return b;
 }
 
 
