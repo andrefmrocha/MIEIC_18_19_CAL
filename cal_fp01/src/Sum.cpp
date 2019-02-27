0cbb6df7eebb9982a976b2b167085b2731434432
andrefmrocha <andrefmrocha@live.com.pt>
2019-02-27 12:43:42 +0000

    Finished Sums exercise

--- cal_fp01/src/Sum.cpp
+++ cal_fp01/src/Sum.cpp
@@ -7,6 +7,39 @@
 
 string calcSum(int* sequence, int size)
 {
-	return "";
+
+    int minSum;
+    std::pair<int, int> mininums[size];
+    int index;
+    for(int i = 1; i <= size; i++)
+    {
+        minSum = 0;
+        for(int j = 0; j < i; j++)
+            minSum += sequence[j];
+
+        index = 0;
+        for(int j = 1; j < size - i + 1; j++)
+        {
+            int sum = 0;
+            for(int k = j; k <= j + i - 1; k++)
+            {
+                sum += sequence[k];
+            }
+            if(sum < minSum)
+            {
+                minSum = sum;
+                index = j;
+            }
+
+        }
+        mininums[i - 1] = make_pair(minSum, index);
+    }
+
+    string result;
+    for(int i = 0; i < size; i++)
+    {
+        result+=to_string(mininums[i].first) + "," + to_string(mininums[i].second) + ";";
+    }
+	return result;
 }
 
